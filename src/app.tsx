e06d8e079603476e635a4ec33d7e118e5fc43673
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-23 19:01:20 -0300

    estrutura inicial

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { router } from './routes'
+
+export default function App(){
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+};
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,8 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Dashboard() {
+  <>
+    <Helmet title="Dashboard" />
+    <h1>Dashboard</h1>
+  </>
+}
