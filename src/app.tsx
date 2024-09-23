9cdb1febfd0c903340c29f7316bfd8bdf4134719
matheustorresdev97 <matheustorresdev@gmail.com>
2024-09-23 19:31:28 -0300

    paginas e components

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,19 @@ import { RouterProvider } from 'react-router-dom'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { router } from './routes'
 
-export default function App(){
+import { ThemeProvider } from './components/theme/theme-provider'
+import { Toaster } from 'sonner'
+
+export default function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+
+        <Toaster richColors />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/sign-in'
-import { Dashboard } from './pages/app/dashboard'
+import { SignUp } from './pages/auth/sign-up'
+
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
@@ -23,6 +27,10 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
